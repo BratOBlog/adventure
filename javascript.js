fffa64b7635f59551dbd9501fe67040d9ed17b9b
BratoCode <bratoblog@gmail.com>
2023-01-14 16:26:17 +0100

    add css sheet and basic opprotunities to the randomizer in js add classes for formatting in html

--- javascript.js
+++ javascript.js
@@ -1,11 +1,53 @@
-let titlePartOne= [1,2,3,4,5,6,7]
+let titlePartOne= [
+"Acolyte",
+"Beast",
+"Blood",
+"Children",
+"Gods",
+"Hand",
+"Jewels",
+"Master",
+"Paths",
+"Plague",
+"Prophecy",
+"Queen",
+"Servant",
+"Shadow",
+"Sign",
+"Song",
+"Sword",
+"Vengeance",
+"Wind",
+"Whisper"
+]
 
 function newQuote() {
     let randomNumber = Math.floor(Math.random() * (titlePartOne.length));
     document.getElementById("titlePartOne").innerHTML = titlePartOne[randomNumber];
 }
 
-let titlePartTwo= [8,9,10,11,12,13,15]
+let titlePartTwo= [
+ "of Chaos",
+"of  Darkness",
+"of Death",
+"of Doom",
+"of Eternity",
+"of Midnight",
+"of the Night",
+"of Silence",
+"of Sorcery",
+"of Sorrow",
+"of the Boreas",
+"of the Deeps",
+"of the Ice",
+"of the Lost",
+"of the Moon",
+"of the Mountain",
+"of the Necropolis",
+"of the Stars",
+"of the Tomb",
+"of the Wastes"
+]
 
 function partTwo() {
     let randomNumber = Math.floor(Math.random() * (titlePartTwo.length));
