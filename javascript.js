2e1f91f1d2c81e8dd8118fa49ccb6419b9234b9b
BratoCode <bratoblog@gmail.com>
2023-01-15 17:30:00 +0100

    add new parts of generator in html, css, js

--- javascript.js
+++ javascript.js
@@ -88,13 +88,88 @@ function mission() {
     document.getElementById("mission").innerHTML = missionPool[randomNumber];
 }
 
+
 let missionSubject = [
 "Artist / Abyss / Bottle",
-"Assassin / Bazaar / Chest"
+"Assassin / Bazaar / Chest",
+"Beggar / Brothel / Cloak",
+"Craftsman / Catacombs / Crown",
+"Dancer / Cavern / Crystal",
+"Eunuch / Fortress / Dagger",
+"Foreigner / Garden / Helm",
+"Harlot / Island / Idol",
+"Merchant / Mansion / Jewel",
+"Noble / Mountain / Lens",
+"Peasant / Oasis / Mask",
+"Pirate / Palace / Meteorite",
+"Poet / Pool / Ring",
+"Priest / Prison / Sarcophagus",
+"Scholar / Sanctuary / Scroll",
+"Servant / Shrine / Seal",
+"Slave / Storehouse / Skull",
+"Sorcerer / Tomb / Sword",
+"Thief / Tower / Tablet",
+"Warrior / Valley / Tome",
 ]
 
 
-function missionSubject() {
+function missionTwo() {
     let randomNumber = Math.floor(Math.random() * (missionSubject.length));
-    document.getElementById("missionSubject").innerHTML = missionSubject[randomNumber];
+    document.getElementById("missionTwo").innerHTML = missionSubject[randomNumber];
 }
+
+let hook = [
+"Ambushed and left for dead",
+"Arrested and pressed into service of local warlord",
+"Asked as a favor",
+"Blackmailed",
+"Cursed by a god",
+"Dared or had courage questioned",
+"Found old map",
+"Geased by a sorcerer",
+"Had a dream or vision",
+"Heard a prophecy",
+"Heard a song or poem",
+"Hired as mercenary",
+"Lost or shipwrecked",
+"Made a wager",
+"Ordered by superior",
+"Overheard conversation",
+"Read ancient scroll or tablet",
+"Repaying a debt",
+"Stumbled into situation",
+"Wronged and seeking revenge"
+]
+
+function hookPrint() {
+    let randomNumber = Math.floor(Math.random() * (hook.length));
+    document.getElementById("hookPrint").innerHTML = hook[randomNumber];
+}
+
+let villainPool = [
+    "Angry ghost",
+    "Bandit chieftain",
+    "Brutal warlord",
+    "Corrupt official",
+    "Cult leader",
+    "Cunning madman",
+    "Debauched noble",
+    "Demonized wretch",
+    "Depraved monk",
+    "Deranged alchemist",
+    "Evil sorcerer",
+    "Fanatical priest",
+    "Greedy merchant",
+    "King of thieves",
+    "Obsessed scholar",
+    "Pirate captain",
+    "Ruthless slaver",
+    "Sadistic torturer",
+    "Sardonic harlequin",
+    "Sinister revenant"
+    ]
+    
+    function villain() {
+        let randomNumber = Math.floor(Math.random() * (villainPool.length));
+        document.getElementById("villain").innerHTML = villainPool[randomNumber];
+    }
