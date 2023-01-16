d523e22efb29c1b7022f4293f349d3b74ed5b264
BratoCode <bratoblog@gmail.com>
2023-01-16 10:38:46 +0100

    add ally part to html, js and css

--- javascript.js
+++ javascript.js
@@ -173,3 +173,30 @@ let villainPool = [
         let randomNumber = Math.floor(Math.random() * (villainPool.length));
         document.getElementById("villain").innerHTML = villainPool[randomNumber];
     }
+
+    let allyPool = [
+        "Aged warrior seeking one last battle",
+        "Charismatic leader of a slave revolt",
+        "Demon seeking the soul of antagonist",
+        "Disgruntled servant of antagonist",
+        "Escaped prisoner",
+        "Friendly witch",
+        "Honorable captain of the guard",
+        "Local ruler whose throne was usurped",
+        "Naïve and virtuous young warrior",
+        "Priest who speaks for the gods",
+        "Rival of antagonist",
+        "Rogue adventurer seeking the same goal",
+        "Strangely intelligent animal",
+        "Streetwise urchin",
+        "Sybil with an important prophecy",
+        "Unworldly scholar with vital information",
+        "Vengeful spirit",
+        "Victim of antagonist seeking revenge",
+        "Villain who wants to be redeemed",
+        "Wise sage with useful lore"
+        ]
+        
+        function ally() {
+            let randomNumber = Math.floor(Math.random() * (allyPool.length));
+            document.getElementById("ally").innerHTML = allyPool[randomNumber];}
